ac7ea108005836b75da5b8057928fe7fa09d3616
psy2848048 <psy2848048@gmail.com>
2025-11-03 22:08:50 +0900

    fix: added fixes

--- complex_main.py
+++ complex_main.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from src.routes.health import health_bp
+from src.routes.token import token_bp
+
+
+app = Flask(__name__)
+
 
 def create_app():
-    app = Flask(__name__)
-
     app.register_blueprint(health_bp)
 
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
